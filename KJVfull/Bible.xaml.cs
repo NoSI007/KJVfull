e6f76287fa3a00f36417e4d97c53d6501dcaef27
khaled <khaled_alwan@hotmail.com>
2022-03-29 12:23:19 +0100

    added outTxt(), to produce text dump of the database in 2 files, for android 2.56M file limit.

--- KJVfull/Bible.xaml.cs
+++ KJVfull/Bible.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace KJVfull
 {
@@ -22,6 +13,37 @@ namespace KJVfull
         public Bible()
         {
             InitializeComponent();
+            //outTxt();
+        }
+
+        private void outTxt()
+        {
+            string ln;
+            var db = KJVData.db;
+
+            var otr = (from ot in db.BSections orderby ot.ID select ot).Take(15000).ToList();
+
+            StreamWriter sw = new StreamWriter("p1.txt");
+            WritIt(otr, sw);
+            //
+            var ntr = (from ot in db.BSections orderby ot.ID select ot).Skip(15000).ToList();
+
+            StreamWriter sw0 = new StreamWriter("p2.txt");
+            WritIt(ntr, sw0);
+
+        }
+
+        private  void WritIt(List<BSection> otr, StreamWriter sw)
+        {
+            string ln;
+            foreach (BSection b in otr)
+            {
+                ln = string.Format("{0}^{1}:{2}^{3}", b.Book, b.Chapter, b.Section, b.Text);
+                sw.WriteLine(ln);
+            }
+            sw.Close();
+            sw.Dispose();
+            
         }
 
         private void xRead_Click(object sender, RoutedEventArgs e)
---
+++ makeJson/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeJson
+{
+    internal class Program
+    {
+        public static kjVDataContext db = new kjVDataContext();
+        static List<json4section> jsondata;
+        static void Main(string[] args)
+        {
+            jsondata  = db.json4sections.ToList();
+
+            //using the newly jasonize table
+            // I can use this to create any table.
+            List<json4section> data = new List<json4section>();
+            string fname;
+            int start = 0;
+            int max = getMaxfrom(start);
+
+            for(int i = 0; ;i++)
+            {
+                fname = String.Format("p{0}.txt", i);
+                StreamWriter sw = new StreamWriter(fname);
+                Console.WriteLine("start({0}) -- max({1})",start,max);
+                data.AddRange(jsondata.Skip(start).Take(max));
+                //
+                process(data,sw);
+                sw.Close();
+                sw.Dispose();
+                //
+                if (start >= jsondata.Count-1)
+                    break;
+                start = max;
+                max = getMaxfrom(start);
+                if(max == 1 ) break;
+                data.Clear();
+            
+            }
+            
+           
+
+            
+
+        }
+        //for now this is not accurate 
+        // if last line just missed 
+        // next line very large..
+        private static int getMaxfrom(int start)
+        {
+            int mx = 0;
+            int ret = 0;
+            if (start == jsondata.Count - 1)
+                return 0;
+
+            for(int i=start;i< jsondata.Count;i++)
+            {
+                mx += jsondata[i].jsont.Length;
+                ret = i;
+                if (mx >= 2500000)
+                    break;
+            }
+            return ret+1;
+        }
+
+        private static void process(List<json4section> data, StreamWriter sw)
+        {
+            sw.WriteLine("[");
+            foreach (var j in data)
+            {
+                sw.WriteLine(j.jsont);
+                sw.Write(";");
+            }
+            sw.WriteLine("]");
+        }
+        /*
+foreach(object s in q1)
+   {
+       ln = s.ToString().Replace("},{", "},\n{");
+       sw.WriteLine(ln);
+   }
+
+*/
+    }
+}
